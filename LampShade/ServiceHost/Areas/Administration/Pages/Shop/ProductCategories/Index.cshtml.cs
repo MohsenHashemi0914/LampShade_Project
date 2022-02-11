155944792cfea2d0545007fcf4454b8578097b72
MohsenHashemi0914 <95424389+MohsenHashemi0914@users.noreply.github.com>
2022-02-11 11:09:53 -0800

    Create ProductCategory implemented

--- LampShade/ServiceHost/Areas/Administration/Pages/Shop/ProductCategories/Index.cshtml.cs
+++ LampShade/ServiceHost/Areas/Administration/Pages/Shop/ProductCategories/Index.cshtml.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using ShopManagement.Application.Contracts.ProductCategory;
 
@@ -23,6 +24,17 @@ namespace ServiceHost.Areas.Administration.Pages.Shop.ProductCategories
         public void OnGet(ProductCategorySearchModel searchModel)
         {
             ProductCategories = _productCategoryApplication.Search(searchModel);
+        }
+
+        public IActionResult OnGetCreate()
+        {
+            return Partial("./Create", new CreateProductCategory());
+        }
+
+        public IActionResult OnPostCreate(CreateProductCategory command)
+        {
+            var result = _productCategoryApplication.Create(command);
+            return new JsonResult(result);
         }
     }
 }
