dd9692cb3607870dacbe74298033add64fd5d0fb
MohsenHashemi0914 <95424389+MohsenHashemi0914@users.noreply.github.com>
2022-02-20 10:12:49 -0800

    Slide added to ShopManagement project

---
+++ LampShade/ShopManagement.Infrastructure.EFCore/Repository/SlideRepository.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using _0_Framework.Infrastructure;
+using ShopManagement.Application.Contracts.Slide;
+using ShopManagement.Domain.SlideAgg;
+
+namespace ShopManagement.Infrastructure.EFCore.Repository
+{
+    public class SlideRepository : BaseRepository<long, Slide>, ISlideRepository
+    {
+        #region Constructor
+
+        private readonly ShopContext _context;
+
+        public SlideRepository(ShopContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        #endregion
+
+        public EditSlide GetDetails(long id)
+        {
+            return _context.Slides.Select(x => new EditSlide
+            {
+                Id = x.Id,
+                Picture = x.Picture,
+                PictureAlt = x.PictureAlt,
+                PictureTitle = x.PictureTitle,
+                Title = x.Title,
+                Heading = x.Heading,
+                Text = x.Text,
+                BtnText = x.BtnText
+            }).FirstOrDefault(x => x.Id == id);
+        }
+
+        public List<SlideViewModel> GetList()
+        {
+            return _context.Slides.Select(x => new SlideViewModel
+            {
+                Id = x.Id,
+                Picture = x.Title,
+                Heading = x.Heading,
+                Title = x.Title,
+                CreationDate = x.CreationDate.ToString(CultureInfo.InvariantCulture),
+                IsRemoved = x.IsRemoved
+            }).OrderByDescending(x => x.Id).ToList();
+        }
+    }
+}
