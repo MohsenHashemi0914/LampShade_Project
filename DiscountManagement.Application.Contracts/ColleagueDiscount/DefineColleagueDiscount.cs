47665a29ee4168a2024103b333be51b707a761ac
MohsenHashemi0914 <95424389+MohsenHashemi0914@users.noreply.github.com>
2022-03-01 12:36:55 -0800

    ColleagueDiscount core implemented

---
+++ DiscountManagement.Application.Contracts/ColleagueDiscount/DefineColleagueDiscount.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using DiscountManagement.Application.Contracts.CustomerDiscount;
+
+namespace DiscountManagement.Application.Contracts.ColleagueDiscount
+{
+    public class DefineColleagueDiscount
+    {
+        public long ProductId { get; set; }
+        public int DiscountRate { get; set; }
+        public List<ProductViewModel> Products { get; set; }
+    }
+}
---
+++ DiscountManagement.Infrastructure.EFCore/Repository/ColleagueDiscountRepository.cs
@@ -0,0 +1,61 @@
+﻿using _0_Framework.Infrastructure;
+using DiscountManagement.Application.Contracts.ColleagueDiscount;
+using DiscountManagement.Domain.ColleagueDiscountAgg;
+using System.Collections.Generic;
+using System.Linq;
+using _0_Framework.Application;
+using ShopManagement.Infrastructure.EFCore;
+
+namespace DiscountManagement.Infrastructure.EFCore.Repository
+{
+    public class ColleagueDiscountRepository : BaseRepository<long, ColleagueDiscount>, IColleagueDiscountRepository
+    {
+        #region Constructor
+
+        private readonly DiscountContext _context;
+        private readonly ShopContext _shopContext;
+
+        public ColleagueDiscountRepository(DiscountContext context, ShopContext shopContext) : base(context)
+        {
+            _context = context;
+            _shopContext = shopContext;
+        }
+
+        #endregion
+
+        public EditColleagueDiscount GetDetails(long id)
+        {
+            return _context.ColleagueDiscounts.Select(x => new EditColleagueDiscount
+            {
+                Id = x.Id,
+                ProductId = x.ProductId,
+                DiscountRate = x.DiscountRate
+            }).FirstOrDefault(x => x.Id == id);
+        }
+
+        public List<ColleagueDiscountViewModel> Search(ColleagueDiscountSearchModel searchModel)
+        {
+            var products = _shopContext.Products.Select(x => new {x.Id, x.Name}).ToList();
+
+            var query = _context.ColleagueDiscounts
+                .Select(x => new ColleagueDiscountViewModel
+                {
+                    Id = x.Id,
+                    ProductId = x.ProductId,
+                    CreationDate = x.CreationDate.ToFarsi(),
+                    IsRemoved = x.IsRemoved
+                });
+
+            if (searchModel.ProductId > 0)
+                query = query.Where(x => x.ProductId == searchModel.ProductId);
+
+            var discounts = query.OrderByDescending(x => x.Id).ToList();
+
+            if (discounts.Any())
+                discounts.ForEach(discount =>
+                    discount.Product = products.FirstOrDefault(x => x.Id == discount.ProductId)?.Name);
+
+            return discounts;
+        }
+    }
+}
